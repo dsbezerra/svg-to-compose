b83b9c47b3ee45fc105980d614beaa42ceadefb8
Diego Bezerra <dsouzaduarte@gmail.com>
2022-06-28 11:54:31 -0300

    Update kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.6.21"
     id("maven-publish")
 }
 
 group = "br.com.devsrsouza"
-version = "0.7.0"
+version = "0.8.2"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ dependencies {
     implementation("com.google.guava:guava:23.0")
     implementation("com.android.tools:sdk-common:27.2.0-alpha16")
     implementation("com.android.tools:common:27.2.0-alpha16")
-    implementation("com.squareup:kotlinpoet:1.9.0")
+    implementation("com.squareup:kotlinpoet:1.12.0")
     implementation("org.ogce:xpp3:1.1.6")
 
     testImplementation(kotlin("test-junit"))
